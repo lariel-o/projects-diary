341ef14bcc15ccd400e3b6f399ab9734d3a94da8
Lucas Ariel <larielolim@gmail.com>
2026-07-15 04:10:15 -0300

    Rebuilding the entiry project by the zero

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"github.com/lariel-o/projects-diary/database"
-	"github.com/lariel-o/projects-diary/database/querys/task"
-)
-
 func main() {
-	database.Init()
-	task.Change(0, 1, true, true, database.Task{
-		Title: "Meu novo titulo",
-	})
 }
 
