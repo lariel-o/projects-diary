60b9a0fe0a3193ac0e96601c6a17b58fbcf50f2c
Lucas Ariel <larielolim@gmail.com>
2026-07-18 04:03:14 -0300

    start making the persistence logic

--- database/main.go
+++ database/main.go
@@ -1,13 +1,57 @@
 package database
 
+import (
+	"fmt"
+	"path/filepath"
+	"os"
+)
+
 type database struct {
 	oProjects []project
 	fProjects []project
 }
+
 var db database
 
 // InitDatabase make and save the database in the volatile memory
-func InitDatabase() {
-	// Load the db from non-volatile memory
+func InitDatabase() error {
+	// ########
+	// Load the db from non-volatile memory 
+
+
+	// Verify if the folder exist
+	userHome, err := os.UserHomeDir()
+	if err != nil {
+		return fmt.Errorf("Error while trying to get the user home", err)
+	}
+
+
+	folderPath := filepath.Join(userHome, ".config", "projects-diary")
+	filePath := filepath.Join(folderPath, "database.json")
+
+
+	// Verify if the "pojects-diary" folder exists
+	if _, err := os.Stat(folderPath); err != nil && os.IsNotExist(err) {
+		// Create a new "projects-diary" folder
+		if err := os.Mkdir(folderPath, 0755); err != nil {
+			return fmt.Errorf("Could not create the project's folder: %w", err)
+		}
+	} 
+
+	// Verify if the database file exists
+	file, err := os.Open(filePath)
+	if err != nil {
+		if os.IsExist(err) {
+			return fmt.Errorf("The file exist but there's something wrong: %w", err)
+		}
+
+		// Create the new database.json file
+		if err := os.WriteFile(filePath, []byte("{}"), 0644); err != nil {
+			return fmt.Errorf("err trying to create database file: %w", err)
+		}
+	}
+
+	defer file.Close()
+	return nil
 }
 
--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"github.com/lariel-o/projects-diary/internal/pages"
+	"fmt"
+	"os"
+
+	// "github.com/lariel-o/projects-diary/internal/pages"
 	"github.com/lariel-o/projects-diary/database"
 
-	"github.com/rivo/tview"
+	// "github.com/rivo/tview"
 )
 
 func main() {
 	// Init the database
-	database.InitDatabase()
-
-	app := tview.NewApplication()
-
-	p := pages.Dashi(app)
-
-	if err := app.SetRoot(p, true).SetFocus(p).Run(); err != nil {
-		panic(err)
+	if err := database.InitDatabase(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	// app := tview.NewApplication()
+	// p := pages.Dashi(app)
+	//
+	// if err := app.SetRoot(p, true).SetFocus(p).Run(); err != nil {
+	// 	panic(err)
+	// }
 }
 
