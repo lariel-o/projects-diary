48ccf01713bc5db200b99f30d2c31742e701f754
Lucas Ariel <larielolim@gmail.com>
2026-07-19 04:57:00 -0300

    Implement the logic to save projects in memory after creating them

--- internal/pages/create.go
+++ internal/pages/create.go
@@ -1,28 +1,42 @@
 package pages
 
 import (
+	"fmt"
+
+	"github.com/lariel-o/projects-diary/internal/database"
+
 	"github.com/rivo/tview"
 )
 
 
 // The create project page
-func createProjectPage() *tview.Grid {
+func createProjectPage(p *tview.Pages) *tview.Grid {
 	form := tview.NewForm()
 
 	form.SetBorder(true).SetTitle(" Create a new project ")
 	form.SetBorderPadding(2, 2, 2, 2) 
 
 	
-	projectNameInput := tview.NewInputField().
-		SetLabel("Project name").
-		SetFieldWidth(90)
-
-
-
-	form.AddFormItem(projectNameInput)
+	form.AddInputField("Project name", "", 90, nil, nil)
 	form.AddTextArea("Description", "", 90, 15, 0, nil)
-	form.AddButton("Submit", func() {})
-	form.AddButton("Quit", func() {})
+	form.AddButton("Submit", func() {
+		nameField := form.GetFormItem(0).(*tview.InputField)
+		descField := form.GetFormItem(1).(*tview.TextArea)
+
+		name := nameField.GetText()
+		desc := descField.GetText()
+
+		if name == "" || desc == "" {
+			// janela de erro popup
+		} else {
+			database.CreateNewProject(name, desc)
+			p.SwitchToPage(fmt.Sprintf("%d", cPROJECT))
+		}
+	})
+
+	form.AddButton("Quit", func() {
+		p.SwitchToPage(fmt.Sprintf("%d", cPROJECT))
+	})
 
 
 	grid := tview.NewGrid().
